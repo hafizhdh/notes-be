23a594942f88cfe1da939064fd755bb7756602ab
Hafiz <hafizadhyaulhaq16@gmail.com>
2024-08-14 23:11:49 +0700

    Init application

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.status(200).json({"message": "Welcome to notes application backend"})
+})
+
+const PORT = process.env.PORT || 8008
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
